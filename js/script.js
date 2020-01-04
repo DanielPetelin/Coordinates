cdd7ea333df3c7a766e47bd0e3df32dcf78c79f3
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-04 12:34:35 +0300

    font

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+// задание отловить координаты мыши относительно блока на экране.
+// и выводить их на экран.
+
+// создаём функцию, которая будет показывать координаты.
 document.querySelector('#test').onmousemove = function(event) {
     event = event || window.event;
+    // console.log(event);
+    // выводим координаты x
+    // на экран с помощью innerHTML.
+    document.querySelector('#offx').innerHTML = event.offsetX;
+    // выводим коорлинаты y
+    // на экран с помощью innerHTML.
+    document.querySelector('#offy').innerHTML = event.offsetY;
 }
